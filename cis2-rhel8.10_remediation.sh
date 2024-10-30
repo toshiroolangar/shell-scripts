553053077352003d8f568c837adb0b4c8facaf5e
toshiroolangar <toshi.olangar04@gmail.com>
2024-10-30 11:44:04 +0800

    Created remediation script for rhel8.10 cis findings

---
+++ cis2-rhel8.10_remediation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+: '
+DISCLAIMER: This script is made specifically for RHEL 8.10. 
+
+This script is created in line with the Amazon Inspector findings for RHEL8.10. This script aims to validate if the operating system passes the Amazon Inspector CIS Benchmark.
+
+Created by: Toshiro Olangar
+'
+echo "hello"
